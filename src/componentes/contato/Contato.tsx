f283fae98572a86d13ebbc269600f7f5279bd065
MariaRocha26 <maria.rocha6@estudante.ifms.edu.br>
2023-10-17 07:58:00 -0400

    atualizando contato

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,23 +1,27 @@
 import './Contato.css'
+import React, {useState} from "react";
+export default function Contato(){
+    const [texto, setTexto] = useState("");
+    function change_txt(e: React.ChangeEvent<HTMLInputElement>){
+        setTexto(e.target.value);
+}
 
-function Contato() {
     return(
         <main className='contato'>
         <section>
             <h2>Formulário para Contato</h2>
             <form action="" method="post">
                 <label>Nome</label>
-                <input type="text" name="name" placeholder="Digite seu nome" autoComplete='off'></input>
+                <input type="text" name="name" placeholder="Digite seu nome" autoComplete='off' onChange={change_txt}></input>
                 <label>Sobrenome</label>
-                <input type="text" name="name" placeholder="Digite seu sobrenome" autoComplete='off'></input>
+                <input type="text" name="name" placeholder="Digite seu sobrenome" autoComplete='off' onChange={change_txt}></input>
                 <label>Email</label>
-                <input type="email" name="email" placeholder="Digite seu email"></input>
+                <input type="email" name="email" placeholder="Digite seu email" onChange={change_txt}></input>
                 <label>Mensagem</label>
-                <textarea name="message" cols={30} rows={10} placeholder="Digite sua mensagem"></textarea>
+                <input type="text" name="message" placeholder="Digite sua mensagem" onChange={change_txt}></input>
                 <button type='submit'>Enviar</button>
             </form>
         </section>
         </main>
     )
 }
-export default Contato
