ce16a04d2db303eb739b89729e411992e082a3f2
MariaRocha26 <maria.rocha6@estudante.ifms.edu.br>
2023-10-17 08:30:23 -0400

    mudando contato

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,10 +1,23 @@
 import './Contato.css'
 import React, {useState} from "react";
 export default function Contato(){
-    const [texto, setTexto] = useState("");
-    function change_txt(e: React.ChangeEvent<HTMLInputElement>){
-        setTexto(e.target.value);
-}
+    const [nome, setNome] = useState("");
+        function mudaTexto0(e: React.ChangeEvent<HTMLInputElement>){
+        setNome(e.target.value);
+    }
+    const [sobrenome, setSobrenome] = useState("");
+        function mudaTexto1(e: React.ChangeEvent<HTMLInputElement>){
+        setSobrenome(e.target.value);       
+    }
+    const [email, setEmail] = useState("");
+        function mudaTexto2(e: React.ChangeEvent<HTMLInputElement>){
+        setEmail(e.target.value);       
+    }
+
+    const [mensagem, setMensagem] = useState("");
+        function mudaTexto3(e: React.ChangeEvent<HTMLInputElement>){
+        setMensagem(e.target.value);       
+    }
 
     return(
         <main className='contato'>
@@ -12,13 +25,17 @@ export default function Contato(){
             <h2>Formulário para Contato</h2>
             <form action="" method="post">
                 <label>Nome</label>
-                <input type="text" name="name" placeholder="Digite seu nome" autoComplete='off' onChange={change_txt}></input>
+                <input type="text" name="name" placeholder="Digite seu nome" autoComplete='off' onChange={mudaTexto0}></input>
+                <p>nome:{nome}</p>
                 <label>Sobrenome</label>
-                <input type="text" name="name" placeholder="Digite seu sobrenome" autoComplete='off' onChange={change_txt}></input>
+                <input type="text" name="name" placeholder="Digite seu sobrenome" autoComplete='off' onChange={mudaTexto1}></input>
+                <p>sobrenome:{sobrenome}</p>
                 <label>Email</label>
-                <input type="email" name="email" placeholder="Digite seu email" onChange={change_txt}></input>
+                <input type="email" name="email" placeholder="Digite seu email" onChange={mudaTexto2}></input>
+                <p>email:{email}</p>
                 <label>Mensagem</label>
-                <input type="text" name="message" placeholder="Digite sua mensagem" onChange={change_txt}></input>
+                <input type="text" name="message" placeholder="Digite sua mensagem" onChange={mudaTexto3}></input>
+                <p>mensagem:{mensagem}</p>
                 <button type='submit'>Enviar</button>
             </form>
         </section>
